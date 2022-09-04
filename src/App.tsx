c53c259f43702013bd1ba8cffc8add7f15f823fa
quibler <ashwinidroid@gmail.com>
2022-09-05 04:09:23 +0530

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import * as anchor from "@project-serum/anchor";
 import Home from "./Home";
 import { DEFAULT_TIMEOUT } from "./connection";
